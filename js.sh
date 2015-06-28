f0ee3815ef4b16bdeeb8a4b9164f48153051ca4b
Andres Suarez <zertosh@gmail.com>
2015-06-28 17:38:05 -0400

    Allow multiple flags and add "--clean"

--- js.sh
+++ js.sh
@@ -17,27 +17,28 @@ NODE_OS=$(uname | tr A-Z a-z)
 NODE_DIR="vendor/$NODE_TYPE-$NODE_VER-$NODE_OS-x64"
 
 if [[ $# == 0 ]]; then
-  echo 'Usage: js.sh node [args]'
-  echo '       js.sh npm [args]'
-  echo '       js.sh MODULE_CLI [args]'
-  echo '       js.sh --bins'
+  echo 'usage: js.sh [--node-bin] [--iojs-bin] [--npm-bin] [--clean]'
+  echo '       js.sh [--clean] [node|npm|MODULE_CLI] [args...]'
   exit 1
 fi
 
-case "$1" in
-  --node-bin)
-    echo $NODE_DIR/bin/node; exit 0;;
-  --iojs-bin)
-    echo $NODE_DIR/bin/iojs; exit 0;;
-  --npm-bin)
-    echo $NODE_DIR/bin/npm; exit 0;;
-  --bins)
-    echo $NODE_DIR/bin/node
-    [[ $NODE_TYPE == iojs ]] && echo $NODE_DIR/bin/iojs
-    echo $NODE_DIR/bin/npm
-    exit 0
-    ;;
-esac
+while test $# -gt 0; do
+  case "$1" in
+    --node-bin) echo $NODE_DIR/bin/node; shift;;
+    --iojs-bin) echo $NODE_DIR/bin/iojs; shift;;
+    --npm-bin)  echo $NODE_DIR/bin/npm; shift;;
+    --clean)
+      find vendor -maxdepth 1 \( -name "iojs-v*" -o -name "node-v*" \) \
+        -exec sh -c 'echo "js.sh: Removing {}" 1>&2; rm -rf "{}"' \;
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+[[ $# == 0 ]] && exit 0
 
 if [[ ! -e $NODE_DIR/bin/node ]] || [[ ! -e $NODE_DIR/bin/npm ]]; then
   if [[ $NODE_TYPE == iojs ]] && [[ $NODE_VER == *"nightly"* ]]; then
@@ -47,7 +48,7 @@ if [[ ! -e $NODE_DIR/bin/node ]] || [[ ! -e $NODE_DIR/bin/npm ]]; then
   elif [[ $NODE_TYPE == node ]]; then
     NODE_URL="https://nodejs.org/dist/$NODE_VER/node-$NODE_VER-$NODE_OS-x64.tar.gz"
   fi
-  echo "Downloading $NODE_URL ..." 1>&2
+  echo "js.sh: Downloading $NODE_URL ..." 1>&2
   mkdir -p $NODE_DIR
   curl $NODE_URL | tar -xz -C $NODE_DIR --strip-components=1
 fi
@@ -61,13 +62,13 @@ MODULE_CLI_BIN="node_modules/.bin/$1"
 
 if [[ -x "$MODULE_CLI_BIN" ]]; then
   shift
-  echo "Using: $NODE_DIR with $MODULE_CLI_BIN" 1>&2;
+  echo "js.sh: NODE_ENV=\"$NODE_ENV\" $NODE_DIR on $MODULE_CLI_BIN" 1>&2
   $MODULE_CLI_BIN $@
 elif [[ -x "$NODE_CMD_BIN" ]]; then
   shift
-  echo "Using: $NODE_CMD_BIN" 1>&2;
+  echo "js.sh: NODE_ENV=\"$NODE_ENV\" $NODE_CMD_BIN" 1>&2
   $NODE_CMD_BIN $@
 else
-  echo "Don't know $1 - what is it?" 1>&2;
+  echo "js.sh: Don't know what \"$1\" is" 1>&2
   exit 1
 fi
